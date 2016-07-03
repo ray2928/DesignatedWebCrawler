cdbc2633328339895387f7da2aca5f4a793f6de6
ray2928 <rxpkd@mail.missouri.edu>
2016-07-02 19:53:09 -0500

    add request header so it will not throw 403 forbidden error

--- spy_test.py
+++ spy_test.py
@@ -8,11 +8,20 @@ class Spider:
 
     def __init__(self):
         #self.siteURL = 'http://mm.taobao.com/json/request_top_list.htm'
-        self.siteURL = 'http://www.1point3acres.com/bbs/forum-28-1.html'
+        #self.siteURL = 'http://www.1point3acres.com/bbs/forum-28-1.html'
+        self.siteURL = 'http://www.mitbbs.com/bbsdoc/JobHunting.html'
     def getPage(self,pageIndex):
         url = self.siteURL + "?page=" + str(pageIndex)
         print url
-        request = urllib2.Request(url)
+        # headers = headers={"Accept" : "text/html"}
+        request_headers = {
+        "Accept-Language": "en-US,en;q=0.5",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Referer": "http://thewebsite.com",
+        "Connection": "keep-alive"
+        }
+        request = urllib2.Request(url, headers = request_headers)
         response = urllib2.urlopen(request)
         print "response"
         print response.read()
