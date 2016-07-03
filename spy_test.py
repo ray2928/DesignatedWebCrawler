71ba147c581a9474f683714f4d19aca96d3966ce
ray2928 <rxpkd@mail.missouri.edu>
2016-07-02 21:07:09 -0500

    Use proxy so that out IP will not be banned

--- spy_test.py
+++ spy_test.py
@@ -6,14 +6,19 @@ import re
 
 class Spider:
 
+# ToDO:
+# - Timeout setting
+
     def __init__(self):
         #self.siteURL = 'http://mm.taobao.com/json/request_top_list.htm'
         #self.siteURL = 'http://www.1point3acres.com/bbs/forum-28-1.html'
         self.siteURL = 'http://www.mitbbs.com/bbsdoc/JobHunting.html'
     def getPage(self,pageIndex):
+        self.setProxy()
         url = self.siteURL + "?page=" + str(pageIndex)
-        print url
-        # headers = headers={"Accept" : "text/html"}
+        print "======================================="
+        print "reading url: ", url
+        print "======================================="
         request_headers = {
         "Accept-Language": "en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",
@@ -23,9 +28,21 @@ class Spider:
         }
         request = urllib2.Request(url, headers = request_headers)
         response = urllib2.urlopen(request)
-        print "response"
-        print response.read()
+        print "================response==============="
+        # print response.read()
         return response.read().decode('gbk')
+
+    def setProxy(self):
+        enable_proxy = True
+        proxy = "http://www.google.com:8080"
+        proxy_handler = urllib2.ProxyHandler({"https" : proxy})
+        null_proxy_handler = urllib2.ProxyHandler({})
+        if enable_proxy:
+            opener = urllib2.build_opener(proxy_handler)
+        else:
+            opener = urllib2.build_opener(null_proxy_handler)
+        urllib2.install_opener(opener)
+        print "proxy has been set to ",proxy
 
     def getContents(self,pageIndex):
         page = self.getPage(pageIndex)
@@ -33,6 +50,8 @@ class Spider:
         items = re.findall(pattern,page)
         for item in items:
             print item[0],item[1],item[2],item[3],item[4]
+        print "======================================="
+        print "Done! Page loaded!"
 
 spider = Spider()
 spider.getContents(1)
