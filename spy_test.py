d0205fa225b1c4da69bdaca6f69168710feeb8d0
ray2928 <rxpkd@mail.missouri.edu>
2016-07-02 20:04:10 -0500

    customised header for mitbbs

--- spy_test.py
+++ spy_test.py
@@ -15,10 +15,10 @@ class Spider:
         print url
         # headers = headers={"Accept" : "text/html"}
         request_headers = {
-        "Accept-Language": "en-US,en;q=0.5",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Referer": "http://thewebsite.com",
+        "Accept-Language": "en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Referer": "http://www.mitbbs.com/bbsboa/1.html",
         "Connection": "keep-alive"
         }
         request = urllib2.Request(url, headers = request_headers)
